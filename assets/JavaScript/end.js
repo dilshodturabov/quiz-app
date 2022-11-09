10275180e316aa8dac2f3f221a796399a79db6d7
dilshodturabov <avlomberdioglid@gmail.com>
2022-11-09 23:01:59 +0500

    fix page control

--- assets/JavaScript/end.js
+++ assets/JavaScript/end.js
@@ -38,5 +38,5 @@ saveHighScore = e => {
   highScores.splice(5);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign('../../index.html');
+  window.location.assign('../../quiz-app/index.html');
 }
